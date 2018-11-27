00f1f45fafdda1d61b0e4412330b05a6b2bf18c0
yafeunteun <yannfeunteun@gmail.com>
2018-11-27 21:36:05 +0100

    script to download dataset

--- get_data.sh
+++ get_data.sh
@@ -2,5 +2,5 @@
 
 mkdir input
 cd input 
-kaggle competitions download -c traveling-santa-2018-prime-paths 
+kaggle competitions download -c traveling-santa-2018-prime-paths && unzip cities.csv.zip
 
