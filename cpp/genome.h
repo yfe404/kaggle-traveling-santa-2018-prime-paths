9976a63f0413b139ba06710531450d82336f0a12
yafeunteun <yannfeunteun@gmail.com>
2018-12-02 16:06:31 +0100

    definition of < operator for genome

--- cpp/genome.h
+++ cpp/genome.h
@@ -17,9 +17,14 @@ class Genome {
   Genome(vector<int> phenotype);
   vector<int> get_phenotype();
 
+  bool operator < (const Genome& otherGenome) const
+  {
+    return (this->fitness > otherGenome.fitness);
+  }
+  
  private:
   vector<int> phenotype; 
-  int fitness;
+  long fitness = 999999999999;
 };
 
 #endif // GENOME_H
