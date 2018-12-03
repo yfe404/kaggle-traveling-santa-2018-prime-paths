c22996c6c7f95a5a86db436bd11b133d89b5a04f
yafeunteun <yannfeunteun@gmail.com>
2018-12-03 12:51:09 +0100

    check cli

--- cpp/crossover.cc
+++ cpp/crossover.cc
@@ -2,12 +2,17 @@
 using namespace std; 
   
 int main(int argc, char** argv) 
-{ 
-    cout << "You have entered " << argc 
-         << " arguments:" << "\n"; 
-  
-    for (int i = 0; i < argc; ++i) 
-        cout << argv[i] << "\n"; 
-  
-    return 0; 
+{
+  // Check the number of parameters
+  if (argc < 3) {
+    // Tell the user how to run the program
+    std::cerr << "Usage: " << argv[0] << " PATH_TO_PARENT1.TSP PATH_TO_PARENT2.TSP" << std::endl;
+    /* "Usage messages" are a conventional way of telling the user
+     * how to run a program if they enter the command incorrectly.
+     */
+    return 1;
+  }
+  std::cout << "Processing genetic sequences....." << endl;
+  return 0;
+
 } 
