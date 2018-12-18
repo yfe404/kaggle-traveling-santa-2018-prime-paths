9166ef3fa29275c921f7572328539b8bfcc65c89
Maxime Mouchet <max@maxmouchet.com>
2018-12-18 19:11:30 +0100

    Update tests and is_prime return type

--- gpu/src/primes.hpp
+++ gpu/src/primes.hpp
@@ -3,18 +3,18 @@
 
 #include <cmath>
 
-int is_prime(int n) {
+bool is_prime(int n) {
     int i;
     if (n < 2)
-        return 0;
+        return false;
     if (n == 2)
-        return 1;
+        return true;
     if (n % 2 == 0)
-        return 0;
+        return false;
     for (i = 3; i <= sqrt((float)n); i += 2)
         if (n % i == 0)
-            return 0;
-    return 1;
+            return false;
+    return true;
 }
 
 #endif
--- gpu/test/test_io.hpp
+++ gpu/test/test_io.hpp
@@ -6,5 +6,11 @@ class TestIO : public CxxTest::TestSuite {
     void testReadCities(void) {
         vector<City> cities = read_cities("../test/cities.csv");
         TS_ASSERT_EQUALS(cities.size(), 197769);
+        TS_ASSERT_EQUALS(cities[2].i, 2);
+        TS_ASSERT_EQUALS(cities[2].p, true);
+        TS_ASSERT_EQUALS(cities[2].xy.x, 3454.15819771172);
+        TS_ASSERT_EQUALS(cities[2].xy.y, 2820.05301124811);
+        TS_ASSERT_EQUALS(cities[197768].i, 197768);
+        TS_ASSERT_EQUALS(cities[197768].p, false);
     }
 };
